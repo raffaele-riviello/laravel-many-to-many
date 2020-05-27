d3f4504f5508cf36ce19fbc46c3fc5b0e83912e6
Raffaele Riviello <raffaele.riviello@gmail.com>
2020-05-27 20:18:37 +0200

    initial setup

--- public/js/app.js
+++ public/js/app.js
@@ -37292,8 +37292,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/chiarapassaro/mamp_public/laravel-mercoledi/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/chiarapassaro/mamp_public/laravel-mercoledi/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel-many-to-many/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/laravel-many-to-many/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
